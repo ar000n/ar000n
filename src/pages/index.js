712cdb50c5ba5efddd79251b432a3c575acf6f3b
Arun Sathian <arunsa@thoughtworks.com>
2019-12-22 20:00:45 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Arun" />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
